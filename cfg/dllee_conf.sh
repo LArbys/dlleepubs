8747d55b5af775a822b07ab2cff455cb1a6f6e79
Victor Genty <vgenty@nevis.columbia.edu>
2018-01-09 23:48:38 -0500

    update vertex reco

---
+++ cfg/dllee_conf.sh
@@ -0,0 +1,55 @@
+# echo "Setup pubs"
+# source /home/vgenty/sw/pubs/config/setup.sh
+# echo "Setup online"
+# source /uboonenew/setup_online.sh
+# echo "Setup gcc"
+# setup gcc v4_9_2
+# echo "Setup python"
+# setup python v2_7_9
+# echo "Setup postgres"
+# setup postgresql v9_3_6 -q p279
+# echo "Setup psycopg"
+# setup psycopg2 v2_5_4
+# echo "Setup git"
+# setup git v2_3_0
+# echo "Setup sam"
+# setup sam_web_client
+# echo "Setup ifdhc"
+# setup ifdhc
+# echo "kx509"
+# kx509
+
+# SQL reader account config
+export PUB_PSQL_READER_HOST=nudot.lns.mit.edu
+export PUB_PSQL_READER_USER=tufts-pubs
+export PUB_PSQL_READER_ROLE=""
+export PUB_PSQL_READER_DB=procdb
+export PUB_PSQL_READER_PASS=""
+export PUB_PSQL_READER_CONN_NTRY=10
+export PUB_PSQL_READER_CONN_SLEEP=10
+
+# SQL writer account config
+export PUB_PSQL_WRITER_HOST=nudot.lns.mit.edu
+export PUB_PSQL_WRITER_USER=tufts-pubs
+export PUB_PSQL_WRITER_ROLE=""
+export PUB_PSQL_WRITER_DB=procdb
+export PUB_PSQL_WRITER_PASS=""
+export PUB_PSQL_WRITER_CONN_NTRY=10
+export PUB_PSQL_WRITER_CONN_SLEEP=10
+
+# SQL admin account config
+export PUB_PSQL_ADMIN_HOST=nudot.lns.mit.edu
+export PUB_PSQL_ADMIN_USER=tufts-pubs
+export PUB_PSQL_ADMIN_ROLE=""
+export PUB_PSQL_ADMIN_DB=procdb
+export PUB_PSQL_ADMIN_PASS=""
+export PUB_PSQL_ADMIN_CONN_NTRY=10
+export PUB_PSQL_ADMIN_CONN_SLEEP=10
+
+# SMTP account for sending an email report
+export PUB_SMTP_ACCT=pupinjesus
+export PUB_SMTP_SRVR=smtp.gmail.com:587
+export PUB_SMTP_PASS=pupinjesus1
+
+export PUB_INDIR=""
+export PUB_OUTDIR=""
---
+++ utils/downstream_utils.py
@@ -0,0 +1,30 @@
+import os
+
+PUB_TOP_DIR = os.environ["PUB_TOP_DIR"]
+DLLEE_DIR   = os.path.join(PUB_TOP_DIR,"dlleepubs")
+VERTEX_DIR  = os.path.join(DLLEE_DIR,"vertex")
+CFG_DIR     = os.path.join(DLLEE_DIR,"downstream","Production_Config","cfg")
+SCRIPT_DIR  = os.path.join(DLLEE_DIR,"downstream","vertex-tuftscluster-scripts","mac","template","base")
+
+
+def cast_run_subrun(run,subrun,
+                    runtag,input_format,
+                    input_dir,output_dir):
+
+    runmod100    = run%100
+    rundiv100    = run/100
+    subrunmod100 = subrun%100
+    subrundiv100 = subrun/100
+    
+    jobtag      = 10000*run + subrun
+    inputdbdir  = os.path.join(input_dir,"%03d/%02d/%03d/%02d/"%(rundiv100,runmod100,subrundiv100,subrunmod100))
+    ssnetinput  = os.path.join(inputdbdir,input_format%("ssnetout-larcv",run,subrun))
+    ssnetinput += ".root"
+    outputdbdir = os.path.join(output_dir,"%s/%03d/%02d/%03d/%02d/"%(runtag,rundiv100,runmod100,subrundiv100,subrunmod100))
+
+    return jobtag, ssnetinput, inputdbdir, outputdbdir
+
+def cmd(SS):
+    print SS
+    os.system(SS)
+    return
